23cf690ab53a96f1be7e039a68b57a423e19af7c
Graf-Lev-V <legendarlev2004@gmail.com>
2026-05-13 00:22:12 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <nav><Link href='/'>Home</Link> <Link href='/about'>About</Link> <Link href='/users'>Users</Link></nav>
+      <body className="min-h-full flex flex-col bg-gray-50 text-gray-900">
+        <nav className="bg-gray-900 px-6 py-4 flex gap-6 items-center">
+          <Link href='/' className="text-white hover:text-gray-300">Home</Link>
+          <Link href='/about' className="text-white hover:text-gray-300">About</Link>
+          <Link href='/users' className="text-white hover:text-gray-300">Users</Link>
+        </nav>
         {children}
       </body>
     </html>
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -27,19 +27,17 @@ export default async function Users() {
 
     return (
         <>
-            <hr/>
-            <h1 className="mx-auto">Users</h1>
+            <h1 className="text-xl font-bold ml-6 mt-4">Users</h1>
             {errorMessage && <p>{errorMessage}</p>}
-            {users.map((user) => 
-                <div key={user.id}>
-                    <hr/>
-                    <p>
-                        <Link href={`/users/${user.id}`}>{user.name}</Link><br/>
-                        {user.email}<br/>
-                        Company: {user.company.name}
-                    </p>
-                </div>
-            )}
+            <div className="grid grid-cols-2 p-6 gap-4">
+                {users.map((user) => 
+                    <div key={user.id} className="bg-gray-200 p-4 rounded-md shadow-md hover:bg-gray-300">
+                        <Link href={`/users/${user.id}`} className="text-blue-600 font-bold text-lg">{user.name}</Link><br/>
+                        <p className="text-gray-600 text-sm">{user.email}</p>
+                        <p className="text-gray-600 text-sm">Company: {user.company.name}</p>
+                    </div>
+                )}
+            </div>
         </>
     )
 }
